7678465d0b458ad561908cd14590fcf573377550
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-21 12:43:45 -0400

    Caching Basics.

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 
-"""BasicCache module."""
+""" BasicCache module.
+"""
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """
-    Basic Cache Class.
+    """ Basic Cache Class.
     """
 
     def put(self, key, item):
-        """Add item."""
+        """ Add item.
+        """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Get item."""
+        """ Get item.
+        """
         theItem = self.cache_data.get(key)
         return theItem
