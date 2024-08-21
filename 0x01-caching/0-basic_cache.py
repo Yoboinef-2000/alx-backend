f3a7123efe980ecf3d4d2d5e6d8e987880b94890
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-21 12:40:46 -0400

    Caching Basics.

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,7 +5,9 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """Basic Cache Class."""
+    """
+    Basic Cache Class.
+    """
 
     def put(self, key, item):
         """Add item."""
@@ -14,6 +16,5 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """Get item."""
-        # if key is None or key not in self.cache_data:
-        #     return None
-        return self.cache_data.get(key)
+        theItem = self.cache_data.get(key)
+        return theItem
