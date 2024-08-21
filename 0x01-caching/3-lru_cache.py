dc4c40d6c157e1c51c187c7dc4d0a045c41b6c26
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-21 15:44:50 -0400

    LRU caching.

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+""" LRUCache module.
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache class.
+    """
+
+    def __init__(self):
+        """ Initialize.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add item using LRU.
+        """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {discarded_key}")
+
+    def get(self, key):
+        """ Get item.
+        """
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            return self.cache_data[key]
+        return None
