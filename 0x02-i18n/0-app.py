9b58bfcb5fa8c244d708e4e7dd872738eb3a6770
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-27 06:16:17 -0400

    Not using babel yet.

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+"""Setting up a basic flask app."""
 from flask import Flask, render_template
 # from flask_babel import Babel
 
@@ -7,9 +8,12 @@ app = Flask(__name__)
 # app.config.from_pyfile('mysettings.cfg')
 # babel = Babel(app)
 
+
 @app.route('/')
 def index():
+    """Home page."""
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
