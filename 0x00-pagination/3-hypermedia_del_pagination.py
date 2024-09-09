283a24da5fba53fe34f9f8bbcf4566753cd7da26
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-09-08 20:04:58 -0400

    Some popular baby names.

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -34,7 +34,7 @@ class Server:
                 {i: dataset[i] for i in range(len(dataset))}
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
         Return a dictionary with the following key-value pairs:
         - index: the current start index of the return page.
